9236695cea6eebb3e1db892ab34cbf09f771bcae
saurabhbadgujar <36645967+saurabhbadgujar@users.noreply.github.com>
2019-05-10 15:01:31 +0530

    Update train.py

--- train.py
+++ train.py
@@ -6,8 +6,8 @@ from gan import Gan
 from support.image_caption_loader import load_image_and_text
 
 def main():
-    img_width = 64
-    img_height = 64
+    img_width = 32
+    img_height = 32
     img_channels = 3
 
     image_path = './dataset/images'
@@ -21,7 +21,7 @@ def main():
     gan.img_width = img_width
     gan.img_height = img_height
     gan.img_channels = img_channels
-    gan.random_input_dim = 100
+    gan.random_input_dim = 20
     gan.glove_path = './glove'
 
     batch_size = 20
